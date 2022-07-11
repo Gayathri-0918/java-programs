bf6d28c1d6b2a113a786a78fd0e41e80e02685de
Gayathri-0918 <108792184+Gayathri-0918@users.noreply.github.com>
2022-07-11 20:19:18 +0530

    Add files via upload

---
+++ Program10.java
@@ -0,0 +1,18 @@
+package newProjects;
+
+public class Program10 {
+
+	public static void main(String[] args) {
+	int a=19;
+	if(a%2==0)
+	{
+		System.out.println("the number is even");
+	}
+	else
+	{
+		System.out.println("the number is odd");
+	}
+
+	}
+
+}
